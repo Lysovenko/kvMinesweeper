ddd481f58a5e2503b1936afb41abcb8ef9fb3ed7
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2022-12-18 23:51:40 +0200

    Autoopen around zeros

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ListProperty
-from minefield import MineField
+from minefield import MineField, Opener
 
 
 class RootWidget(GridLayout):
@@ -12,37 +12,37 @@ class RootWidget(GridLayout):
         super(RootWidget, self).__init__(**kwargs)
         self.cols = 10
         self.rows = 10
-        minefield = MineField()
+        minefield = Opener(MineField())
+        self.btns = []
         for i in range(100):
-            btn = CustomBtn(text="?")
+            btn = CustomBtn(text="")
             btn.btn_r = i // 10
             btn.btn_c = i % 10
             btn.app_mf = minefield
+            btn.change_neibs = self.change_btns
             self.add_widget(btn)
+            self.btns.append(btn)
 
-    def btn_pressed(self, instance, pos):
-        print('pos: printed from root widget: {pos}'.format(pos=pos))
+    def change_btns(self, btns):
+        for r, c, v in btns:
+            p = r * self.cols + c
+            try:
+                self.btns[p].text = str(v)
+            except IndexError:
+                pass
 
 
 class CustomBtn(Button):
 
-    pressed = ListProperty([0, 0])
-    calc_down = 3
-
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.pressed = touch.pos
-            v = self.app_mf.get(self.btn_r, self.btn_c)
+            v = self.app_mf.pick(self.btn_r, self.btn_c)
             self.text = "Boom!" if v is None else str(v)
-            self.calc_down -= 1
+            if v == 0:
+                neibs = self.app_mf.around_zeros()
+                self.change_neibs(neibs)
         return super().on_touch_down(touch)
-
-    def on_pressed(self, instance, pos):
-        if self.calc_down <= 0:
-            print("Remove myself")
-            print(self.get_root_window())
-            print(self.get_parent_window())
-            self.get_root_window().remove_widget(self)
 
 
 class MinesweeperApp(App):
--- minefield.py
+++ minefield.py
@@ -6,25 +6,73 @@ class MineField:
         indices = list(range(rows * cols))
         rinds = [indices.pop(randint(0, len(indices) - 1))
                  for i in range(mines)]
-        self.field = [[0 for j in range(cols)] for i in range(rows)]
+        self.__field = [[0 for j in range(cols)] for i in range(rows)]
         self.rows = rows
         self.cols = cols
         for i in rinds:
             row = i // cols
             col = i % cols
-            self.field[row][col] = None
+            self.__field[row][col] = None
         for r in range(rows):
             for c in range(cols):
-                if self.field[r][c] is None:
+                if self.__field[r][c] is None:
                     continue
                 for nr in (-1, 0, 1):
                     for nc in (-1, 0, 1):
                         if any((nr != 0, nc != 0)):
                             try:
-                                if self.field[r + nr][c + nc] is None:
-                                    self.field[r][c] += 1
+                                if self.__field[r + nr][c + nc] is None:
+                                    self.__field[r][c] += 1
                             except IndexError:
                                 continue
 
     def get(self, row, col):
-        return self.field[row][col]
+        return self.__field[row][col]
+
+
+class Opener:
+    def __init__(self, field):
+        self.__mined = field
+        self.rows = field.rows
+        self.cols = field.cols
+        self.__opened = [[None for j in range(self.cols)]
+                         for i in range(self.rows)]
+
+    def pick(self, row, col):
+        v = self.__mined.get(row, col)
+        if v is None:
+            self.__opened[row][col] = "B"
+        else:
+            self.__opened[row][col] = v
+        return v
+
+    def _around_zeros(self):
+        neibours = []
+        for r in range(self.rows):
+            for c in range(self.cols):
+                if self.__opened[r][c] == 0:
+                    for rr in (-1, 0, 1):
+                        for rc in (-1, 0, 1):
+                            if all((rr == 0, rc == 0)):
+                                continue
+                            if any((rr + r < 0, rc + c < 0)):
+                                continue
+                            try:
+                                if self.__opened[r + rr][c + rc] is None:
+                                    v = self.__mined.get(r + rr, c + rc)
+                                    if v is None:
+                                        v = "B"
+                                    self.__opened[r + rr][c + rc] = v
+                                    neibours.append((r + rr, c + rc, v))
+                            except IndexError:
+                                continue
+        return neibours
+
+    def around_zeros(self):
+        neibours = []
+        while True:
+            neibs = self._around_zeros()
+            if not neibs:
+                break
+            neibours.extend(neibs)
+        return neibours
