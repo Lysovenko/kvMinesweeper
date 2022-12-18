8f9b451e582fde537a33904517bba223babe811d
Serhii Lysovenko <Lysovenko@users.noreply.github.com>
2022-12-18 15:10:49 +0200

    first boom

--- __main__.py
+++ __main__.py
@@ -3,6 +3,8 @@ from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ListProperty
+from minefield import MineField
+
 
 class RootWidget(GridLayout):
 
@@ -10,35 +12,38 @@ class RootWidget(GridLayout):
         super(RootWidget, self).__init__(**kwargs)
         self.cols = 10
         self.rows = 10
-        self.add_widget(Button(text='btn 1'))
-        cb = CustomBtn(text="X")
-        cb.bind(pressed=self.btn_pressed)
-        self.add_widget(cb)
-        self.add_widget(Button(text='btn 2'))
-        for i in range(97):
-            self.add_widget(CustomBtn(text=str(i)))
+        minefield = MineField()
+        for i in range(100):
+            btn = CustomBtn(text="?")
+            btn.btn_r = i // 10
+            btn.btn_c = i % 10
+            btn.app_mf = minefield
+            self.add_widget(btn)
 
     def btn_pressed(self, instance, pos):
         print('pos: printed from root widget: {pos}'.format(pos=pos))
+
 
 class CustomBtn(Button):
 
     pressed = ListProperty([0, 0])
     calc_down = 3
+
     def on_touch_down(self, touch):
         if self.collide_point(*touch.pos):
             self.pressed = touch.pos
-            self.text = str(int(touch.pos[0]))
+            v = self.app_mf.get(self.btn_r, self.btn_c)
+            self.text = "Boom!" if v is None else str(v)
             self.calc_down -= 1
         return super().on_touch_down(touch)
 
     def on_pressed(self, instance, pos):
-        print('pressed at {pos}'.format(pos=pos))
         if self.calc_down <= 0:
             print("Remove myself")
             print(self.get_root_window())
             print(self.get_parent_window())
             self.get_root_window().remove_widget(self)
+
 
 class MinesweeperApp(App):
 
